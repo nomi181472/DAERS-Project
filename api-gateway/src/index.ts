ead2478f0f350495d6620a4bf9c415ef6d1fe9f0
nomi181472 <nomansoomro52@gmail.com>
2020-10-29 01:34:33 +0500

    Routing policy File Added

--- api-gateway/src/index.ts
+++ api-gateway/src/index.ts
@@ -1,20 +1,11 @@
 import express from "express";
 import {json} from "body-parser"
+const route=require("./routing-policy");
 //import axios from "axios";
 const app =express();
 app.use(json());
+app.use("/",route);
 app.listen(3000,()=>{
-    console.log("api-gateway listening to port number 3111!!");
-});
-app.get("/apigateway/current",(req,res)=>{
-    console.log("hello");
-res.send("hi There");
-
-});
-app.get("/",(req,res)=>{
-
-    console.log("hello");
-res.send("hi There");
-
+    console.log("api-gateway listening to port number 3000!!");
 });
 
