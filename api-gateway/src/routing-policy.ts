e215bb3f296232bdc708aa0263aae97a59c7b05b
nomi181472 <nomansoomro52@gmail.com>
2020-10-30 12:32:46 +0500

    modified:   src/routing-policy.ts

--- api-gateway/src/routing-policy.ts
+++ api-gateway/src/routing-policy.ts
@@ -1,6 +1,15 @@
 import express from "express";
 const router=express.Router();
-router.get("/",(req,res)=>{
-    res.send("Routing-policy");
-})
+router.get("/api-gateway/user/*",(req,res)=>{
+    res.send("Routing-policy->get->user");
+});
+router.post("/api-gateway/user/*",(req,res)=>{
+    const path=get_path(req.originalUrl)  
+    res.send(req.originalUrl+" path replaced "+path);
+});
+
+const get_path=(url:string)=>{
+        return url.replace("/api-gateway","");    
+   
+}
 module.exports=router;
